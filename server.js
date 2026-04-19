9fef16c6081b1d8ae973a58d13ce77b6202503d8
valkor-sec <dagb.mat@gmail.com>
2026-04-19 15:35:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -11,27 +10,27 @@ const taskRoutes = require("./routes/task");
 const app = express();
 
 // Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 // DEBUG ENV
 console.log("MONGO_URI =", process.env.MONGO_URI);
 
-// Route test
+// Test route
 app.get("/", (req, res) => {
     res.send("API Todo-Valk fonctionne 🚀");
 });
 
-// Routes API
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-// MongoDB
+// MongoDB connect
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connecté"))
 .catch(err => console.log("Mongo error:", err));
 
-// Server
+// Server start
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
